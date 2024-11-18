977b8ad1699462b5c0e8a72e729a5a0e8dd12a2e
Chris Lutze <clutze@gmail.com>
2024-11-17 22:09:40 -1000

    added comments

--- adminsite/admin-site/src/app/forms/page.tsx
+++ adminsite/admin-site/src/app/forms/page.tsx
@@ -17,11 +17,11 @@ interface FormField {
 
 // Type defintion for FromTemplate, matches backend Model
 interface FormTemplate {
-  form_id: number;
+  form_id?: number;
   created_at?: string;
   description: string;
   created_by?: string;
-  project_id?: number;
+  project_id: number;
   fields: FormField[];
 }
 
@@ -36,13 +36,14 @@ export default function Page() {
     'radio',
     'checkbox',
   ]
-
+  
+  // Add a new field to the form builder
   function addField() {
     setFormFields([
       ...formFields,
       {
         field_id: formFields.length + 1,
-        field_title: `Field ${formFields.length + 1}`,
+        field_title: '',
         field_type: 'TextInput',
         field_order: formFields.length + 1,
         field_description: '',
@@ -53,18 +54,21 @@ export default function Page() {
     ]);
   }
 
+  // Update form field value(s) in the form builder
   function updateField(index: number, key: keyof FormField, value: FormField[keyof FormField]) {
     const newFields = [...formFields];
     newFields[index] = { ...newFields[index], [key]: value };
     setFormFields(newFields);
   }
 
+  // Delete a field from the form builder
   function removeField(index: number) {
     const newFields = [...formFields];
     newFields.splice(index, 1);
     setFormFields(newFields);
   };
 
+  // Move a field up or down in the form builder
   const moveField = (index: number, direction: 'up' | 'down') => {
     if ((direction === 'up' && index === 0) || 
           (direction === 'down' && index === formFields.length -1)) 
@@ -78,9 +82,10 @@ export default function Page() {
     setFormFields(newFields);
   };
 
+  // Generate FormTemplate JSON from the form
   function generateJSON() {
     const template: FormTemplate = {
-      form_id: 1, //TODO need to get this id from the backend?
+      project_id: 1, //TODO need to get this id from the backend
       description: formTitle,
       fields: formFields
     }
@@ -100,6 +105,8 @@ export default function Page() {
           placeholder="Enter Form Title"
           />
       </div>
+
+      {/*Dynamically display form field editing sections*/}
       {formFields.map((field, index) => (
         <div key={field.field_id}>
           {/*Title, move and delete buttons*/}
@@ -123,20 +130,21 @@ export default function Page() {
           </div>
           {/*Field Title*/}
           <div>
-            <label>Label</label>
+            <label>Field Label</label>
             <input 
               value={field.field_title}
-              placeholder="Field label"
+              placeholder="Enter field label"
               onChange={(e) => updateField(index, 'field_title', e.target.value)}/>
           </div>
-          {/*Field Placeholder*/}
+          {/*Field Description*/}
           <div>
-            <label>Placeholder</label>
+            <label>Description</label>
             <input 
               value={field.field_description}
-              placeholder="Field placeholder"
+              placeholder="Enter field description"
               onChange={(e) => updateField(index, 'field_description', e.target.value)}/>
           </div>
+          {/*Required Checkbox*/}
           <div>
             <label>Required</label>
             <input 
