6aaedac5cd254e853f19bddfac7e51e8f0bfc3f0
Steven Schaerer <schaeres@oregonstate.edu>
2024-11-23 07:58:54 -0600

    Made the project details form work
    Made the project details form work

--- mobile-app/app/project-description.tsx
+++ mobile-app/app/project-description.tsx
@@ -1,23 +1,47 @@
 import { Image, StyleSheet, Text } from "react-native";
 import { View } from "react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useLocalSearchParams, router } from "expo-router";
 
 // Create a Project Description component for student mobile project description page
 export default function ProjectDescription() {
   // useState Hook to manage student code input
   const [projectCode, onChangeProjectCode] = useState("");
+  const { id } = useLocalSearchParams();
+  const [project, setProject] = useState<any | null>(null);
+
+  useEffect(() => {
+    async function getProject() {
+      const response = await fetch(
+        `https://capstone-deploy-production.up.railway.app/project/${id}`,
+        {
+          credentials: "include",
+          method: "GET",
+        }
+      );
+
+      if (!response.ok) {
+        return;
+      }
+
+      const data = await response.json();
+
+      setProject(data.project);
+    }
+
+    getProject();
+  }, [id]);
+
   return (
     <View style={[styles.homeContainer]}>
       <header style={styles.header}>Citizen Science App</header>
       <main style={styles.main}>
         Project Description Page
-        <div>Welcome to Project #1!</div>
-        <Text>Hello students and welcome to week 1 of "Growing a Tree"!</Text>
-        <Text>
-          Week 1: To get started, we will quickly review our tree varieties from
-          class...
-        </Text>
-        <Text>Week 2: This week you will...</Text>
+        <div>{project && project.title}</div>
+        <Text>Description:</Text>
+        <Text>{project && project.description}</Text>
+        <Text>Instructions:</Text>
+        <Text>{project && project.instructions}</Text>
       </main>
     </View>
   );
@@ -28,12 +52,14 @@ const styles = StyleSheet.create({
   homeContainer: {
     backgroundColor: "#dcd5be",
     minHeight: "100%",
+    alignItems: "center",
   },
   header: {
     backgroundColor: "#a368eb",
     fontSize: 40,
     color: "#ffffff",
     textAlign: "center",
+    width: "100%",
   },
   main: {
     display: "flex",
@@ -42,5 +68,6 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     fontSize: 30,
     gap: 20,
+    maxWidth: 400,
   },
 });
